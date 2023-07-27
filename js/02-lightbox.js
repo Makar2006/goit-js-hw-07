6a92f5e0a7fcd53939dfa1f6c602d548006285dc
Makar2006 <makar.diakiv@gmail.com>
2023-07-27 12:14:48 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const ulEl = document.querySelector('.gallery');
+createMarkup();
+
+function createMarkup() {
+  const markup = galleryItems
+    .map(
+      item => `<li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+         <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+   </li>`
+    )
+    .join('');
+  ulEl.innerHTML = markup;
+}
+
+ulEl.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsPosition: 'bottom',
+    captionsData: 'alt',
+    captionsDelay: 250,
+  });
+}
